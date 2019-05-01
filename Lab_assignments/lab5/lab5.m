1402e922e07c49dbc1f6e47c9272f3e836e65f31
Samantha H. Sun <sunh20@uw.edu>
2019-05-01 15:17:33 -0700

    mid-lab update

--- Lab_assignments/lab5/lab5.m
+++ Lab_assignments/lab5/lab5.m
@@ -67,10 +67,11 @@ IC_set = [10, 30, 100, 300, 1000]; % for A1_0, A2_0
 % 3. tau solver w/ relative tol = 0.001
 % 4. exact solver
 
-tspan = [0 20];
+tspan = [0 1];
 t_eff = zeros(length(IC_set),4); % num ICs x 4 solvers
 
 for i = 1:length(IC_set)
+    fprintf('Working on loop %d/5\n',i)
     IC(1) = IC_set(i);
     IC(4) = IC_set(i);
     
@@ -91,14 +92,121 @@ for i = 1:length(IC_set)
     
     % exact
     tic
-    [t,y] = DSDEleap(@TCellRXN,tspan,IC,params);
+    [t,y] = DSDEexact(@TCellRXN,tspan,IC,params);
     t_eff(i,4) = toc;
 end
+disp('Finished')
+% save("t_eff.mat", 't_eff') % did this once so save it just in case
 
+% plot log time vs. A1_0 and A2_0 value
+t_max = max(max(t_eff));
 
+figure;
+plot(IC_set,log(t_eff),'-o')
+legend('tau @ 0.1','tau @ 0.01','tau @ 0.001','exact')
+xlabel('Initial Value of A1,A2')
+ylabel('log(time elapsed)')
+title('Measuring efficiency of stochastic solvers')
 
+%% Accuracy
+clear all; close all; clc
 
+% set A1, A2 IC to 100, use each solver and run as many times in 5 mins
+% histogram of A1_p with 10 bins
+% fraction of simulations: all on, all on, partially on, error
+% avg, std of A1_p at end
 
+% parameters
+load("params.mat")
+IC(1) = 100;
+IC(4) = 100;
+tspan = [0 20];
+
+A1_p_end = cell(4,1); % save A1_p end values
+A1_p_end{1} = zeros(20000,1);  % cannot be more than this value, determ.
+A1_p_end{2} = zeros(10000,1);   % using shortest t_eff for A1 = 100
+A1_p_end{3} = zeros(5000,1);    % 1- exact, 2- tau0.1, 3- tau0.01
+A1_p_end{4} = zeros(20,1);      % 4- tau0.001
+end_index = zeros(4,1);
+end_time = 5; % seconds
+
+% for each solver
+for solver = 1:4
+    i = 0;
+    switch solver
+        case 1 % exact
+            disp('Using exact solver...')
+            tic
+            while toc < end_time % seconds
+                % update index
+                i = i+1; 
+
+                % solve for system
+                [t,y] = DSDEexact(@TCellRXN,tspan,IC,params);
+
+                % save A1_p(end)
+                A1_p_end{solver}(i) = y(end,2);
+            end
+        case 2 % tau 0.1]
+            disp('Using tau 0.1 solver...')
+            tic
+            while toc < end_time % seconds
+                % update index
+                i = i+1; 
+
+                % solve for system
+                [t,y] = DSDEtauleap(@TCellRXN,tspan,IC,0.1,params);
+
+                % save A1_p(end)
+                A1_p_end{solver}(i) = y(end,2);
+            end
+        case 3 % tau 0.01
+            disp('Using tau 0.01 solver...')
+            tic
+            while toc < end_time % seconds
+                % update index
+                i = i+1; 
+
+                % solve for system
+                [t,y] = DSDEtauleap(@TCellRXN,tspan,IC,0.01,params);
+
+                % save A1_p(end)
+                A1_p_end{solver}(i) = y(end,2);
+            end
+        case 4 % tau 0.001
+            disp('Using tau 0.001 solver...')
+            tic
+            while toc < end_time % seconds
+                % update index
+                i = i+1; 
+
+                % solve for system
+                [t,y] = DSDEtauleap(@TCellRXN,tspan,IC,0.001,params);
+
+                % save A1_p(end)
+                A1_p_end{solver}(i) = y(end,2);
+            end
+    end
+end
+disp('Finished')
+
+% 
+% solver = 1;
+% i = 0;
+% tic
+% while toc < 5 % seconds
+%     % update index
+%     i = i+1; 
+%     
+%     % solve for system
+%     [t,y] = DSDEexact(@TCellRXN,tspan,IC,params);
+%     
+%     % save A1_p(end)
+%     A1_p_end{solver}(i) = y(end,2);
+% end
+% % save idx so we know where to stop indexing
+% end_index(1) = i;
+% disp('Finished')
 
 
 
